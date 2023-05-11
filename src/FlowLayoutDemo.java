b3331446c0976d094f5d3cff1103b5b53b1c409d
aninhaferreira <124297002+aninhaferreira@users.noreply.github.com>
2023-05-11 15:42:40 -0300

    Add files via upload
    Aula 11/05/23
    layoutContainer Slides 197-201

---
+++ src/FlowLayoutDemo.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class FlowLayoutDemo{
+    public void main(String [] args){
+        FlowLayoutFrame flowLayoutFrame = new FlowLayoutFrame();
+        flowLayoutFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        flowLayoutFrame.setSize(300,75);
+        flowLayoutFrame.setVisible(true);
+    }
+}
